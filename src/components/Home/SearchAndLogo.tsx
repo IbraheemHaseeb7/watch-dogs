e2d7921a5de2f49799620c99cdd0548a0665d25e
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2024-01-04 16:39:02 +0500

    minute fixes

--- src/components/Home/SearchAndLogo.tsx
+++ src/components/Home/SearchAndLogo.tsx
@@ -1,6 +1,12 @@
 import Search from "../Gerneral/Search";
 
-export default function SearchAndLogo({ imgLink }: { imgLink: string }) {
+export default function SearchAndLogo({
+  buttonText,
+  imgLink,
+}: {
+  buttonText: string;
+  imgLink: string;
+}) {
   return (
     <div className="flex justify-center items-center w-full relative top-[15rem]">
       <img
@@ -8,7 +14,7 @@ export default function SearchAndLogo({ imgLink }: { imgLink: string }) {
         alt="HomePage Logo"
         className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-0"
       />
-      <Search buttonText="Generate" />
+      <Search buttonText={buttonText} />
     </div>
   );
 }
